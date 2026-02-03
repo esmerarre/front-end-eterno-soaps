d1a3c713a2ebb2ad3df6dcec44e9a88631da7681
Jesica Guallpa <guallpajesica65@gmail.com>
2026-02-02 16:50:01 -0800

    recharts works and fake data is rendering

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,12 +1,24 @@
+import "./AdminDashboard.css";
+import MonthlySalesChart from "../components/SalesChart";
+import TopSellingProductsChart from "../components/TopSellingProductsChart";
+
 interface AdminDashboardProps {
     onAdminSignOut?: () => void;
 }
 
+
 export default function AdminDashboard({onAdminSignOut}: AdminDashboardProps) {
   return (
     <section className="admin-dashboard">
-      <h1>Admin Dashboard</h1>
-      <button onClick={onAdminSignOut}>Sign Out</button>
+        <div className="admin-header">
+            <h1>Admin Dashboard</h1>
+            <button className="sign-out-button" onClick={onAdminSignOut}>Sign Out</button>
+        </div>
+        < div className="admin-content">
+            <MonthlySalesChart />
+            <TopSellingProductsChart />
+        </div>
+
     </section>
   );
 }
