d9ef22d17daa04dccdbd216980484a814dcc87ab
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2026-02-04 19:23:50 -0800

    product catalog table created

--- src/components/InventoryManager.tsx
+++ src/components/InventoryManager.tsx
@@ -15,7 +15,37 @@ export default function InventoryManager({ createNewProduct, createNewVariant, p
       
             <div className="new-product-form-container">
             <h2>Add New Product</h2>
+            <div className="manage-products-section">
+                <div className="product-form-wrapper">
             <NewProductForm createNewProduct={createNewProduct} />
+                </div>
+            <div className="products-catalog-container">
+            <table className="products-catalog">
+                <thead>
+                <tr>
+                    <th>All Products</th>
+                </tr>
+                </thead>
+                <tbody>
+                {products.length === 0 ? (
+                    <tr>
+                    <td colSpan={1}>No products available</td>
+                    </tr>
+                ) : (
+                    products.map((product, index) => {
+ 
+                    return (
+                        <tr className="">
+                        <td>{product.name}</td>
+                        </tr>
+                    );
+                    })
+                )}
+                </tbody>
+            </table>
+            </div>
+            </div>
+            <h2>Add New Variant</h2>
             <AddVariantForm createNewVariant={createNewVariant} products={products} />
         </div>
         </div>
